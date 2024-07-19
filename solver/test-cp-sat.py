c67b10d6749b1c1f1ac56e4cf620fb5626e8c6b6
Amir Abolhasani <amiravtar@gmail.com>
2024-07-19 15:41:23 +0330

    feat: Moved solver into a class

--- solver/test-cp-sat.py
+++ solver/test-cp-sat.py
@@ -1,13 +1,16 @@
-from ortools.sat.python import cp_model
-from dataclasses import dataclass
+import logging
 from collections import namedtuple
+from dataclasses import dataclass
 from itertools import groupby
 from operator import attrgetter
+
+from ortools.sat.python import cp_model
 
 # from data_min import COURSES
 # from data_min2 import COURSES
 from data_2 import COURSES
 
+logger = logging.getLogger()
 DURATION_PER_UNIT = 45
 
 
@@ -95,7 +98,15 @@ class printer(cp_model.CpSolverSolutionCallback):
                 if self.save_last:
                     self.last_sol.append((k, v))
                     continue
-
+                print(
+                    k[0],
+                    k[1].group,
+                    k[1].day,
+                    minutes_to_time(k[1].start),
+                    minutes_to_time(k[1].end),
+                    k[1].prof,
+                    k[1].prefered,
+                )
                 sol.append(
                     [
                         k[0],
@@ -111,116 +122,138 @@ class printer(cp_model.CpSolverSolutionCallback):
             self.sol_list.append((sol, sum([x[-1] for x in sol])))
 
 
+class ModelSolver:
+    def __init__(self, data: dict[int, Course], num_solution: int = 50) -> None:
+        self.data = data
+        self.num_solution = num_solution
+        self.soloutins = []
+
+    def solve(self) -> list[list[tuple[int, TimeProf]]]:
+        self.soloutins.clear()
+        # struct
+        # prof:3 day:1 start:4
+        model = cp_model.CpModel()
+        # Model the problem
+        interval_variables: dict[tuple[int, TimeProf], cp_model.IntervalVar] = {}
+        bool_variables: dict[tuple[int, TimeProf], cp_model.IntVar] = {}
+        bool_variables_prefered: list[cp_model.IntVar] = []
+        # Creat all Variables
+        for k, v in self.data.items():
+            course_times = list()
+            for time in v.times:
+                bool_variables[(k, time)] = model.new_bool_var(
+                    f"bool_course_{k}_day_{time.day}_start_{time.start}_end_{time.end}_prof_{time.prof}"
+                )
+                if time.prefered:
+                    bool_variables_prefered.append(bool_variables[(k, time)])
+                interval_variables[(k, time)] = model.NewOptionalIntervalVar(
+                    start=int(str(time.day + 1) + str(time.start)),
+                    size=time.end - time.start,
+                    end=int(str(time.day + 1) + str(time.end)),
+                    is_present=bool_variables[(k, time)],
+                    name=f"bool_course_{k}_day_{time.day}_start_{time.start}_end_{time.end}_prof_{time.prof}",
+                )
+                course_times.append(bool_variables[(k, time)])
+            model.add_exactly_one(course_times)
+
+        # Creat Group constraints
+        group_data: dict[int, list[Course]] = {
+            group: list(items)
+            for group, items in groupby(
+                sorted(list(self.data.values()), key=attrgetter("group")),
+                key=attrgetter("group"),
+            )
+        }
+        for k, v in group_data.items():
+            group_intervals = []
+            for course in v:
+                for time in course.times:
+                    group_intervals.append(interval_variables[(course.id, time)])
+            demands = [1] * len(group_intervals)
+            model.add_cumulative(group_intervals, demands, 1)
+
+        # Creat professor constraints
+        professor_data: dict[int, list[tuple[TimeProf, int]]] = {}
+        for prof, items in groupby(
+            sorted(
+                [
+                    (time, course[0])
+                    for course in self.data.items()
+                    for time in course[1].times
+                ],
+                key=lambda x: x[0].prof,
+            ),
+            key=lambda x: x[0].prof,
+        ):
+            professor_data[prof] = list(items)
+        for prof_id, val in professor_data.items():
+            professor_intervals = []
+            for time, coruse_id in val:
+                professor_intervals.append(interval_variables[(coruse_id, time)])
+            demands = [1] * len(professor_intervals)
+            model.add_cumulative(professor_intervals, demands, 1)
+
+        model.maximize(sum(bool_variables_prefered))
+        solver = cp_model.CpSolver()
+        # Enumerate all solutions.
+        # solver.parameters.enumerate_all_solutions = True
+        sol_print = printer(bool_variables, interval_variables, model)
+        # sol_print = printer(bool_variables, interval_variables, model, save_last=False)
+        stat = solver.solve(model)
+        for i in range(self.num_solution):
+            if stat == cp_model.INFEASIBLE:
+                logger.info("INFEASIBLE")
+                break
+            if stat == cp_model.MODEL_INVALID:
+                logger.info("MODEL_INVALID")
+            if stat == cp_model.UNKNOWN:
+                logger.info("UNKNOWN")
+                break
+            # if stat in [cp_model.OPTIMAL, cp_model.FEASIBLE]:
+            #     print("optimal,fes")
+            stat = solver.solve(model, sol_print)
+            sol_vars = list()
+            last_sol = list()
+            if len(sol_print.last_sol) != len(data):
+                raise
+            for (id, timeprof), variable in bool_variables.items():
+                if solver.value(variable):
+                    sol_vars.append(variable)
+                    last_sol.append((id, timeprof))
+            # for k, v in sol_print.last_sol:
+            #     print(
+            #         k[0],
+            #         k[1].group,
+            #         k[1].day,
+            #         minutes_to_time(k[1].start),
+            #         minutes_to_time(k[1].end),
+            #         k[1].prof,
+            #         k[1].prefered,
+            #     )
+            #     sol_vars.append(v)
+            model.add(sum(sol_vars) <= len(sol_vars) - 1)
+            # print(solver.user_time)
+            # print(solver.wall_time)
+            # print(sol_print.count)
+            self.soloutins.append(last_sol)
+        return self.soloutins
+
+
 data = parse_courses(COURSES)
 
-# struct
-# prof:3 day:1 start:4
-model = cp_model.CpModel()
-# Model the problem
-interval_variables: dict[tuple[int, TimeProf], cp_model.IntervalVar] = {}
-bool_variables: dict[tuple[int, TimeProf], cp_model.IntVar] = {}
-bool_variables_prefered: list[cp_model.IntVar] = []
-# Creat all Variables
-for k, v in data.items():
-    course_times = list()
-    for time in v.times:
-        bool_variables[(k, time)] = model.new_bool_var(
-            f"bool_course_{k}_day_{time.day}_start_{time.start}_end_{time.end}_prof_{time.prof}"
-        )
-        if time.prefered:
-            bool_variables_prefered.append(bool_variables[(k, time)])
-        interval_variables[(k, time)] = model.NewOptionalIntervalVar(
-            start=int(str(time.day + 1) + str(time.start)),
-            size=time.end - time.start,
-            end=int(str(time.day + 1) + str(time.end)),
-            is_present=bool_variables[(k, time)],
-            name=f"bool_course_{k}_day_{time.day}_start_{time.start}_end_{time.end}_prof_{time.prof}",
-        )
-        course_times.append(bool_variables[(k, time)])
-    model.add_exactly_one(course_times)
+Mo = ModelSolver(data=data, num_solution=100)
 
-# Creat Group constraints
-group_data: dict[int, list[Course]] = {
-    group: list(items)
-    for group, items in groupby(
-        sorted(list(data.values()), key=attrgetter("group")), key=attrgetter("group")
-    )
-}
-for k, v in group_data.items():
-    group_intervals = []
-    for course in v:
-        for time in course.times:
-            group_intervals.append(interval_variables[(course.id, time)])
-    demands = [1] * len(group_intervals)
-    model.add_cumulative(group_intervals, demands, 1)
-
-# Creat professor constraints
-professor_data: dict[int, list[tuple[TimeProf, int]]] = {}
-for prof, items in groupby(
-    sorted(
-        [(time, course[0]) for course in data.items() for time in course[1].times],
-        key=lambda x: x[0].prof,
-    ),
-    key=lambda x: x[0].prof,
-):
-    professor_data[prof] = list(items)
-for prof_id, val in professor_data.items():
-    professor_intervals = []
-    for time, coruse_id in val:
-        professor_intervals.append(interval_variables[(coruse_id, time)])
-    demands = [1] * len(professor_intervals)
-    model.add_cumulative(professor_intervals, demands, 1)
-
-model.maximize(sum(bool_variables_prefered))
-
-solver = cp_model.CpSolver()
-# Enumerate all solutions.
-solver.parameters.enumerate_all_solutions = True
-
-# sol_print = printer(bool_variables, interval_variables, model)
-sol_print = printer(bool_variables, interval_variables, model, save_last=True)
-stat = solver.solve(model, sol_print)
-
-# with open("out.txt", "w") as file:
-#     for i in sorted(sol_print.sol_list,key=lambda x: x[1],reverse=True):
-#         file.write(str(i) + "\n")
-for i in range(80):
-    stat = solver.solve(model, sol_print)
-    # stat = solver.solve(
-    #     model,
-    # )
-
-    if stat in [cp_model.OPTIMAL, cp_model.FEASIBLE]:
-        print("optimal,fes")
-    if stat == cp_model.INFEASIBLE:
-        print("INFEASIBLE")
-        break
-    if stat == cp_model.MODEL_INVALID:
-        print("MODEL_INVALID")
-    if stat == cp_model.UNKNOWN:
-        print("UNKNOWN")
-    sol_vars = list()
-    if len(sol_print.last_sol) != len(data):
-        print("not a sol")
-        bound = solver.BestObjectiveBound()
-
-        print(f"chaning objective from {bound} to {bound-1}")
-        # model.clear_objective()
-        model.add(sum(bool_variables_prefered) <= int(bound) - 1)
-        # model.maximize(sum(bool_variables_prefered))
-        continue
-    for k, v in sol_print.last_sol:
-        print(
-            k[0],
-            k[1].group,
-            k[1].day,
-            minutes_to_time(k[1].start),
-            minutes_to_time(k[1].end),
-            k[1].prof,
-            k[1].prefered,
-        )
-        sol_vars.append(v)
-    model.add(sum(sol_vars) <= len(sol_vars) - 1)
-    print(solver.user_time)
-    print(solver.wall_time)
-    print(sol_print.count)
+if __name__ == "__main__":
+    sols = Mo.solve()
+    for i in sols:
+        for j in i:
+            print(
+                j[0],
+                j[1].group,
+                j[1].day,
+                minutes_to_time(j[1].start),
+                minutes_to_time(j[1].end),
+                j[1].prof,
+                j[1].prefered,
+            )
+        print("\n\n\n")
