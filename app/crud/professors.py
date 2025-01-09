38496bfc73170e161a7393bb39a160b5c78e1fec
Amir Abolhasani <amiravtar@gmail.com>
2025-01-09 18:09:26 +0330

    fix: Fix key constraints

--- app/crud/professors.py
+++ app/crud/professors.py
@@ -1,13 +1,17 @@
 from typing import List, Optional
 
-from sqlalchemy.orm import Session
-from sqlmodel import select
+from fastapi import HTTPException
+from sqlmodel import Session, select
 
+from app.crud.major import get_major
 from app.models.professor import Professor
 from app.schemas.professors import ProfessorCreate, ProfessorUpdate
 
 
 def create_professor(session: Session, professor_data: ProfessorCreate) -> Professor:
+    major = get_major(session, professor_data.major_id)
+    if not major:
+        raise HTTPException(status_code=404, detail="major not found")
     professor = Professor(**professor_data.model_dump())
     session.add(professor)
     session.commit()
@@ -21,7 +25,7 @@ def get_professor(session: Session, professor_id: int) -> Optional[Professor]:
 
 def list_professors(session: Session) -> List[Professor]:
     query = select(Professor)
-    return session.exec(query).all() # type: ignore
+    return session.exec(query).all()  # type: ignore
 
 
 def update_professor(
@@ -30,7 +34,10 @@ def update_professor(
     professor = get_professor(session, professor_id)
     if not professor:
         return None
-
+    if update_data.major_id is not None:
+        major = get_major(session, update_data.major_id)
+        if not major:
+            raise HTTPException(status_code=404, detail="major not found")
     for key, value in update_data.model_dump(exclude_unset=True).items():
         setattr(professor, key, value)
 
