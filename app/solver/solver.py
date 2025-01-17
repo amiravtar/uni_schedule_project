8a843b6da248bb8faa253ab8e268bd812e3f757a
Amir Abolhasani <amiravtar@gmail.com>
2025-01-17 15:44:12 +0330

    feat: Add Classroom constraints

--- app/solver/solver.py
+++ app/solver/solver.py
@@ -16,106 +16,6 @@ from app.schemas.solver import SolverSettings, convert_time_to_min
 # from data_min2 import COURSES
 
 logger = logging.getLogger()
-DURATION_PER_UNIT = 45
-
-
-TimeProf = namedtuple(
-    "TimeProf",
-    ["day", "start", "end", "prof", "duration", "original_start", "group", "prefered"],
-)
-
-
-@dataclass
-class Course:
-    id: str
-    group: int
-    times: list[TimeProf]
-    unit: int
-
-
-def parse_courses(courses: list[tuple[str, list[str], int]]) -> dict[str, Course]:
-    data: dict[str, Course] = dict()
-    for i in courses:
-        c = Course(id=i[0], group=i[2], times=list(), unit=3)
-        time_slotes_set = set()
-        for j in i[1]:
-            day, start, end, prof, prefered = j.split(",")
-            day = int(day)
-            start = int(start)
-            end = int(end)
-            prefered = int(prefered)
-            min_start = convert_time_to_min(start)
-            min_end = convert_time_to_min(end)
-            duration = end - start
-            timeprof = TimeProf(
-                day=day,
-                start=min_start,
-                end=min_end,
-                prof=prof,
-                duration=duration,
-                original_start=start,
-                group=i[2],
-                prefered=prefered,
-            )
-            time_slotes_set.add(timeprof)
-        if len(time_slotes_set) < len(i[1]):
-            raise
-        c.times.extend(time_slotes_set)
-        data[c.id] = c
-    return data
-
-
-# unused
-# class printer(cp_model.CpSolverSolutionCallback):
-#     """Print intermediate solutions."""
-
-#     def __init__(
-#         self,
-#         bool_variables: dict[tuple[int, TimeProf], cp_model.IntVar],
-#         interval_variables: dict[tuple[int, TimeProf], cp_model.IntervalVar],
-#         model,
-#         save_last: bool = True,
-#     ):
-#         super().__init__()
-#         self.bool_variables = bool_variables
-#         self.interval_variables = interval_variables
-#         self.count = 0
-#         self.model: cp_model.CpModel = model
-#         self.last_sol = list()
-#         self.save_last = save_last
-#         self.sol_list = list()
-
-#     def on_solution_callback(self):
-#         self.count += 1
-#         self.last_sol.clear()
-#         sol = list()
-#         for k, v in self.bool_variables.items():
-#             if self.value(v):
-#                 if self.save_last:
-#                     self.last_sol.append((k, v))
-#                     continue
-#                 print(
-#                     k[0],
-#                     k[1].group,
-#                     k[1].day,
-#                     minutes_to_time(k[1].start),
-#                     minutes_to_time(k[1].end),
-#                     k[1].prof,
-#                     k[1].prefered,
-#                 )
-#                 sol.append(
-#                     [
-#                         k[0],
-#                         k[1].group,
-#                         k[1].day,
-#                         minutes_to_time(k[1].start),
-#                         minutes_to_time(k[1].end),
-#                         k[1].prof,
-#                         k[1].prefered,
-#                     ]
-#                 )
-#         if len(sol) == 4:
-#             self.sol_list.append((sol, sum([x[-1] for x in sol])))
 
 
 class ModelSolver:
@@ -141,6 +41,7 @@ class ModelSolver:
         model = cp_model.CpModel()
         # Model the problem
         # the actual time intervals for each course time slots, it is then linked to bool_variables
+        courses_dict = {course.id: course for course in self.data}
         interval_variables: dict[
             tuple[int, SolverCourseTimeSlot], cp_model.IntervalVar
         ] = {}
@@ -153,6 +54,7 @@ class ModelSolver:
             course_times = list()
             if len(course.time_slots) == 0:
                 raise ValueError(f"Course {course} dose not have any timeslot")
+            # create the bool and intvars from the courses and add them to the list
             for time in course.time_slots:
                 bool_variables[(course.id, time)] = model.new_bool_var(
                     name=f"bool_course_{course.id}_day_{time.day}_start_{time.start_time}_end_{time.end_time}_prof_{time.prof}_prefered_{time.prefered}"
@@ -169,13 +71,14 @@ class ModelSolver:
                     name=f"bool_course_{course.id}_day_{time.day}_start_{str(time.start_time)}_end_{str(time.end_time)}_prof_{time.prof}_prefered_{time.prefered}",
                 )
                 course_times.append(bool_variables[(course.id, time)])
+            # select exactly 1 time per course‌ (basically you have to select a time for a course)
             model.add_exactly_one(course_times)
 
         # Creat Group constraints (only 1 class blonging to a group (group_id calculated by semester and major id) can happen at a time)
         group_data: dict[int, list[SolverCourse]] = {
             group: list(items)
             for group, items in groupby(
-                sorted(self.data, key=attrgetter("id")),
+                sorted(self.data, key=attrgetter("group_id")),
                 key=attrgetter("group_id"),
             )
         }
@@ -209,9 +112,8 @@ class ModelSolver:
             demands = [1] * len(professor_intervals)
             model.add_cumulative(professor_intervals, demands, 1)
 
-        # Min and max course hours for professor constraints
+        # Min and max course hours for professor constraints, ensures is the professor has min max hourse > 0, that they are added to model constraints
         if self.settings.professor_min_max_time_limitation:
-            courses_dict = {course.id: course for course in self.data}
             for prof_id, val in professors_data.items():
                 professor_data: ProfessorRead = self.professors[prof_id]
                 prof_min_hour = professor_data.min_hour
@@ -234,14 +136,29 @@ class ModelSolver:
                         sum(bool_var * hour for bool_var, hour in zip(bool_vars, hours))
                         >= prof_min_hour * 100
                     )
-
-        # maxumize for prefered time slots
+        # Add constraints on maximum number on classes that need a spesicif classroom, like computer or architechture classrooms.
+        if self.settings.classroom_limitation:
+            classroom_data: dict[int, list[SolverCourse]] = {
+                group: list(items)
+                for group, items in groupby(
+                    sorted(self.data, key=attrgetter("classroom_id")),
+                    key=attrgetter("classroom_id"),
+                )
+            }
+            for courses in classroom_data.values():
+                classroom_intervals = list()
+                max_classes = 0
+                for course in courses:
+                    max_classes = course.max_classes
+                    for time in course.time_slots:
+                        classroom_intervals.append(  # noqa: PERF401
+                            interval_variables[(course.id, time)]
+                        )
+                demands = [1] * len(classroom_intervals)
+                model.add_cumulative(classroom_intervals, demands, max_classes)
+        # maxumize for prefered time slots, maximize the perefered time by the professors
         model.maximize(sum(bool_variables_prefered))
         solver = cp_model.CpSolver()
-        # solver.parameters.enumerate_all_solutions = True
-        # sol_print = printer(bool_variables, interval_variables, model)
-        # sol_print = printer(bool_variables, interval_variables, model, save_last=False)
-
         for i in range(self.settings.number_of_solutions):
             stat = solver.solve(model)
             if stat == cp_model.INFEASIBLE:
@@ -263,19 +180,6 @@ class ModelSolver:
                     sol_vars.append(model_bool_variable)
                     # TODO :Add score to it, 50 is just a placeholder
                     last_sol.append((id, solver_timeslot, 50))
-            # for k, v in sol_print.last_sol:
-            #     print(
-            #         k[0],
-            #         k[1].group,
-            #         k[1].day,
-            #         minutes_to_time(k[1].start),
-            #         minutes_to_time(k[1].end),
-            #         k[1].prof,
-            #         k[1].prefered,
-            #     )
-            #     sol_vars.append(v)
-            # remove the solution from the next possible solutions
-            # by setting that, next solution cant have all the time slots of past solutions on, meaning at least 1 time slot has to differ
             model.add(sum(sol_vars) <= len(sol_vars) - 1)
             print(solver.user_time)
             self.soloutins.append(last_sol)
@@ -284,29 +188,3 @@ class ModelSolver:
 
 if __name__ == "__main__":
     pass
-    # from mock_data import MockData
-
-    # mock = MockData(
-    #     num_courses=700,
-    #     proffesor_cout=500,
-    #     group_max=70,
-    #     timeslot_per_course_min=5,
-    #     timeslot_per_course_max=12,
-    # )
-    # input_data = mock.generate_data()
-    # data = parse_courses(input_data)
-
-    # Mo = ModelSolver(data=data, num_solution=20)
-    # sols = Mo.solve()
-    # for i in sols:
-    #     for j in i:
-    #         print(
-    #             j[0],
-    #             j[1].group,
-    #             j[1].day,
-    #             minutes_to_time(j[1].start),
-    #             minutes_to_time(j[1].end),
-    #             j[1].prof,
-    #             j[1].prefered,
-    #         )
-    #     print("\n\n\n")
